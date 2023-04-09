bc5f91f18744e7d696a515712e48330db3f1d9b5
WonderSheep <291719281@qq.com>
2023-04-10 02:35:53 +0800

    完成

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react';
 import style from './index.module.scss'//不需安装依赖，直接引用即可
+import { Modal } from 'antd';
 
 
-function Header(){
+function Header() {
 
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const showModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleOk = () => {
+        setIsModalOpen(false);
+    };
+
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
 
     return (
         <div className={style.header}>
             <div>
                 <a href="/">
-                    <img src={require('../../assets/image/cnodejs.png')} alt="网站Logo"/>
+                    <img src={require('../../assets/image/cnodejs.png')} alt="网站Logo" />
                 </a>
-                <span>关于</span>
+                <span onClick={showModal}>关于</span>
             </div>
+            <Modal title="关于本项目" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+                <p>技术栈：</p>
+                <ul>
+                    <li>React</li>
+                    <li>React Router</li>
+                    <li>Redux</li>
+                    <li>Ant Design</li>
+                    <li>Axios</li>
+                </ul>
+            </Modal>
         </div>
     );
 }
